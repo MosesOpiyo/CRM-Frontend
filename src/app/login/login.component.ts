f360be89e5cd03d8c506d4f2ce65fd231a6f29e1
MosesOpiyo <mosesopiyo54@gmail.com>
2023-07-31 23:56:46 +0300

    Login changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { AccountService } from '../AccountService/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(public service:AccountService){}
+
+  email: any;
+  password: any;
+  profile:any;
+  
+
+  loginUser(){
+    let form = new FormData();
+    form.append('username',this.email),
+    form.append('password',this.password),
+    this.service.login(form)
+  }
+  
+  ngOnInit(): void {
+    
+  }
 
 }
